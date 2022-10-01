d855db12c630e05528adb795408b084a9cf8b301
Aamreen <56421592+18121A05K1@users.noreply.github.com>
2022-10-01 21:43:36 +0530

    Update Game.java
    Updated winning condition

--- Game.java
+++ Game.java
@@ -4,7 +4,10 @@ public class Puzzle {
 public static void main(String[] args) {  
 System.out.prinln("game");
 int a=1;
-if(a==1)
+if(a==1){
   System.out.println("Game done");
+  System.out.print("Won!");
+}
+  
 }
 }
