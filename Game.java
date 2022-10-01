d5cdc72e61f072c4577369ea2c9c5c2c13cf2761
Aamreen <56421592+18121A05K1@users.noreply.github.com>
2022-10-01 22:16:00 +0530

    Update Game.java
    Added win-loss condition

--- Game.java
+++ Game.java
@@ -4,5 +4,13 @@ public class Puzzle {
 public static void main(String[] args) {  
 System.out.prinln("game");
 int a=1;
+if(a==1){
+  System.out.prinln("won!");
+
+}
+  else{
+    System.out.prinln("try again");
+
+  }
 }
 }
